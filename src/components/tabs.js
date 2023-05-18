64d248eb741ff4aaa562448e6d1dfe95c75b3808
Joseph Noblett <noblettjoe@gmail.com>
2023-05-18 15:41:41 -0500

    Task 3: created element, appened them and classes

--- src/components/tabs.js
+++ src/components/tabs.js
@@ -13,6 +13,22 @@ const Tabs = (topics) => {
   //   <div class="tab">technology</div>
   // </div>
   //
+
+  const tabWrapper = document.createElement('div');
+  const jsTab = document.createElement('div');
+  const bsTab = document.createElement('div');
+  const techTab = document.createElement('div');
+
+  tabWrapper.appendChild(jsTab);
+  tabWrapper.appendChild(bsTab);
+  tabWrapper.appendChild(techTab);
+
+  tabWrapper.classList.add('topics');
+  jsTab.classList.add('tab');
+  bsTab.classList.add('tab');
+  techTab.classList.add('tab');
+
+  return tabWrapper; // forgot to do this on header lol
 }
 
 const tabsAppender = (selector) => {
